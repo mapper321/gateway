7e8851b0f689ba0fca1bc394a3db1d56be699638
mapper <547008022@qq.com>
2020-04-09 16:13:31 +0800

    first commit

---
+++ src/main/java/com/hebei/Application.java
@@ -0,0 +1,21 @@
+package com.hebei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: mapper
+ * @since: 2020/4/9
+ */
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+//        new SpringApplicationBuilder(Application.class).web(WebApplicationType.NONE).run(args);
+        SpringApplication.run(Application.class, args);
+    }
+}
